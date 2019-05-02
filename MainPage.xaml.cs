7767c71638312975f37364571f3ec2e700daf87e
Billy1 <42975359+GISzone3@users.noreply.github.com>
2019-05-02 18:16:40 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ReschedulingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Clicked1(object sender, EventArgs e)
+        {
+
+            //(YesReschedule.Text);
+
+            /*bool yes = string.IsNullOrEmpty(YesReschedule.Text);
+            if (yes)
+            {
+                Navigation.PushAsync(new ReschedulingPage());
+            }*/
+
+            //Navigation.PushAsync(new ReschedulingPage());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            //(YesReschedule.Text);
+
+            /*bool yes = string.IsNullOrEmpty(YesReschedule.Text);
+            if (yes)
+            {
+                Navigation.PushAsync(new ReschedulingPage());
+            }*/
+
+            Navigation.PushAsync(new ReschedulingPage());
+        }
+    }
+}
---
+++ ReschedulingPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ReschedulingApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ReschedulingPage : ContentPage
+	{
+		public ReschedulingPage ()
+		{
+			InitializeComponent ();
+		}
+        //ZRM4JM
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            bool isDay = string.IsNullOrEmpty(Month.Text);
+            bool isMonth = string.IsNullOrEmpty(Day.Text);
+            bool isYear = string.IsNullOrEmpty(Year.Text);
+            bool isReliever = string.IsNullOrEmpty(Reliever.Text);
+
+            if ((isDay && isMonth && isYear && isReliever) == true)
+            {
+
+            }
+
+            else
+            {
+                DisplayAlert("Please re-enter one of the inputs");
+                //Console.ReadLine();
+            }
+          
+        }
+    }
+}
